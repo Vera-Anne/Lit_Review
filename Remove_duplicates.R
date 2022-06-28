3fb4971e85e9799f496d74b86bc4951847ed9da8
Vera Vinken (PGR) <c0070955@newcastle.ac.uk>
2022-06-28 12:34:55 +0100

    Changes for the latest search: scopus had different column titles. & implemented str_squish

--- Remove_duplicates.R
+++ Remove_duplicates.R
@@ -8,6 +8,7 @@
 
 ##        to do       ## 
   # refine symbols taken out 
+  # preferably upload files to github and run everything from there 
   
 
 
@@ -22,9 +23,10 @@ library(readxl)
 library(gtools)
 require(openxlsx)
 library(data.table)
+library(stringr)
 
 # working directory -> set to where the files are 
-setwd('\\\\webfolders.ncl.ac.uk@SSL/DavWWWRoot/rdw/ion02/02/smulderslab/VeraVinken/1-PHD_PROJECT/Literature review/Searches/2022_06_23')
+setwd('\\\\webfolders.ncl.ac.uk@SSL/DavWWWRoot/rdw/ion02/02/smulderslab/VeraVinken/1-PHD_PROJECT/Literature review/Searches/2022_06_28')
 
 filenames<-list.files(getwd())                            # get the file names 
 
@@ -39,11 +41,18 @@ filenames<-list.files(getwd())                            # get the file names
 
 # put everything in a list 
 filenames<-list.files(getwd())                            # set up the new filenames 
-list_dataframes<-lapply(filenames, read.csv)              # read in the csv files and put in list  
+filenames_new<-{}                                         # create empty  object 
+for (i in 1:length(filenames)){
+  if (grepl("csv", filenames[i], fixed=TRUE)){
+    filenames_new<-cbind(filenames_new,filenames[i])      # create list that only has csv files in it (not the RIS)
+  }}
+
+
+list_dataframes<-lapply(filenames_new, read.csv)              # read in the csv files and put in list  
 
 # Give every dataframe a column with the file name (so we can identify searches)
     for (i in 1:(length(list_dataframes))){  
-      list_dataframes[[i]]$search_ID<-filenames[[i]]
+      list_dataframes[[i]]$search_ID<-filenames_new[[i]]
     }
 
 # Only keep relevant columns 
@@ -68,7 +77,7 @@ for (df in 1:length(list_dataframes)){
     # Check if this dataframe is Scopus  
     if (grepl("scopus",list_dataframes[[df]][1,(ncol(list_dataframes[[df]]))], fixed=TRUE)){
       # if proquest, keep the following: 
-      keep<-c("Title", "Authors", "year", "search_ID")
+      keep<-c("Title", "ï..Authors", "Year", "search_ID")
       list_dataframes[[df]]<-list_dataframes[[df]][,keep]
     } # end of Scopus  
 
@@ -90,7 +99,7 @@ for (col in 1:ncol(combined_data)){
 
 
 # create a vector with all the weird symbols 
-symbols<-cbind("[_]", "[-]", "[,]", '[.]', "[.]", "[©.*]", "[']","[‘]", "[:]", "[;]", "[<>]", "[?]", "[[:space:]]", "[^[:graph:]]", "[()]", "[^[:alnum:]]", '[[:digit:]]+')
+symbols<-cbind("[_]", "[-]", "[,]", '[.]', "[.]", "[©.*]", "[']","[‘]", "[:]", "[;]", "[<>]", "[?]", "[^[:graph:]]", "[()]", "[^[:alnum:]]", '[[:digit:]]+', "[[:space:]]")
 #symbols<-cbind("[_]", "[-]", "[,]", '[.]', "[.]", "[©.*]", "[']","[‘]", "[:]", "[;]", "[<>]", "[?]", "[<i>]", "[</i>]")
 
 for (s in 1:(length(symbols))){
@@ -100,6 +109,7 @@ for (s in 1:(length(symbols))){
 
 # Trim white space at the lead and tail of string
 combined_data$title<-trimws(combined_data$title)
+combined_data$title<-str_squish(combined_data$title)
 # create df with unique titles 
 unique_titles<-combined_data[!duplicated(combined_data$title),]
 
